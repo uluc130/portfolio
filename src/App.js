03ff67c261d23458a73e14d0d13d21b438d156f8
Uluc Hosgelen <uluchosgelen@gmail.com>
2024-01-11 17:50:04 +0300

    new

--- src/App.js
+++ src/App.js
@@ -22,10 +22,15 @@ function App() {
 
 	return (
 		<div className="App">
-			
-			<p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+		<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="*" element={<Notfound />} />
+			</Routes>
 		</div>
 	);
 }
