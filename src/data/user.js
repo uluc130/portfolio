6701cb2c387f77e8a4e1c6e7d3559b0313808d2a
Uluc Hosgelen <uluchosgelen@gmail.com>
2024-01-12 12:06:44 +0300

    new

--- src/data/user.js
+++ src/data/user.js
@@ -3,7 +3,7 @@ const INFO = {
 		title: "Reactfolio by truethari",
 		name: "Uluc Hosgelen",
 		email: "mail@tharindu.dev",
-		logo: "../logo.png",
+		logo: "../twitter.png",
 	},
 
 	socials: {
@@ -12,11 +12,11 @@ const INFO = {
 		linkedin: "https://linkedin.com/",
 		instagram: "https://instagram.com/uluc130/",
 		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		facebook: "https://facebook.com/"
 	},
 
 	homepage: {
-		title: "Full-stack IOS mobile app developer, and triathlete.",
+		title: "Full-stack IOS developer, and triathlete.",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
